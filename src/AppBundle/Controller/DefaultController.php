1402edc12e13ad081a50a6a2aa21b784658356f7
Samer Machara <smachara@gmail.com>
2017-06-22 16:53:34 +0200

    get json

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,20 +2,42 @@
 
 namespace AppBundle\Controller;
 
+use ModelBundle\Entity\User;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Serializer;
+use \Symfony\Component\Serializer\Normalizer;
 
 class DefaultController extends Controller
 {
     /**
      * @Route("/", name="homepage")
+     * @Method("GET")
      */
     public function indexAction(Request $request)
     {
-        // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
-        ]);
+        $em = $this->getDoctrine()->getManager();
+        $users = $em->getRepository(User::class)->findAll();
+
+        return $this->get_json($users);
+
+
     }
+
+     public function get_json($data){
+        $normalizer = array (new Normalizer\GetSetMethodNormalizer());
+        $encoder = ["json"=>new JsonEncoder()];
+        $serializer = new Serializer($normalizer,$encoder);
+        $json = $serializer->serialize($data,'json');
+
+        $response = new \Symfony\Component\HttpFoundation\Response();
+        $response->setContent($json);
+        $response->headers->set("Content-Type", "application/json");
+        return $response;
+    }
+
 }
--- src/ModelBundle/Entity/Video.php
+++ src/ModelBundle/Entity/Video.php
@@ -77,6 +77,7 @@ class Video
      */
     private $updated_at;
 
+
     /**
      * Get id
      *
